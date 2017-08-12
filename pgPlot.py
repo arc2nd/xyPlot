c96a028321a20e558a7cdd17add5d01b89b10321
james <james_parks@hotmail.com>
2017-08-11 22:40:14 -0700

    objectified pgPlot.py, will replace completely later

--- pgPlot.py
+++ pgPlot.py
@@ -101,12 +101,12 @@ def pgPlot(options):
         if options.graphType.lower() == "line":
             #line_graph = pygal.Line()
             line_graph = pygal.XY(config, dynamic_print_values=True, show_x_guides=options.grid, show_y_guides=options.grid, x_label_rotation=45)
-            print('label: {0}'.format(str(options.label)))
+            #print('label: {0}'.format(str(options.label)))
             line_graph.title = str(options.label)
             for this_key in data_dict:
-                print('{0}: {1}'.format(this_key, data_dict[this_key][0]))
-                print('{0}: {1}'.format(this_key, data_dict[this_key][1]))
-                print('len x: {0}. len y: {1}'.format(len(data_dict[this_key][0]), len(data_dict[this_key][1])))
+                #print('{0}: {1}'.format(this_key, data_dict[this_key][0]))
+                #print('{0}: {1}'.format(this_key, data_dict[this_key][1]))
+                #print('len x: {0}. len y: {1}'.format(len(data_dict[this_key][0]), len(data_dict[this_key][1])))
     
                 if '.sample' in this_key.lower():
                     from scipy.interpolate import splev, splrep
@@ -133,6 +133,7 @@ def pgPlot(options):
                 svg_str = line_graph.render()
             except:
                 print(sys.exc_info())
+
         if options.graphType.lower() == 'date':
             from datetime import datetime
             date_graph = pygal.DateTimeLine(config, 
@@ -188,7 +189,7 @@ def pgPlot(options):
                     x2 = np.linspace(xValues[0], xValues[-1], 200)
                     y2 = splev(x2, tck)
 
-                    #convert my interpvalues back into datetime objects
+                    #convert my interp values back into datetime objects
                     xy2 = []
                     i=0
                     interp_pnts = []
@@ -210,6 +211,168 @@ def pgPlot(options):
         fp.write(svg_str)
         fp.close()
         print('wrote: {0}'.format(svg_path))
+
+
+class pgPlotter(object):
+    def __init__(self, graph_type='line', label=None, width=8, height=5, legend=True):
+        self.graph_type = graph_type.lower()
+        self.label = label
+        self.legend = legend
+        self.width = width
+        self.height = height
+        self.svg_str = ''
+        self.grid_x = True
+        self.grid_y = True
+        self.write_to_file = False
+        self.verbosity = 3
+        self.data_dict = {}
+
+        ##prep the graph config
+        self.config = pygal.Config()
+        self.config.show_legend = self.legend 
+        self.config.width = int(self.width) * 100
+        self.config.height = int(self.height) * 100
+
+    def _log(self, local_verbosity, comment):
+        if local_verbosity <= self.verbosity:
+            print(comment)
+
+    def load_json(self, filepath):
+        with open(filepath, 'r') as fp:
+            self.data_dict = json.load(fp)
+
+    def render_to_file(self, filepath):
+        with open(filepath, 'w+') as fp:
+            fp.write(self.svg_str)
+        self._log(1, 'wrote: {0}'.format(filepath))
+
+    def render_to_string(self):
+        return self.svg_str
+
+    def date_graph(self, input_json=None):
+        from datetime import datetime
+        self.load_json(input_json)
+        date_graph = pygal.DateTimeLine(self.config, 
+            dynamic_print_values=True, 
+            show_x_guides=self.grid_x, 
+            show_y_guides=self.grid_y, 
+            x_label_rotation=40, 
+            truncate_label=-1, 
+            x_value_formatter=lambda dt: dt.strftime("%Y-%m-%d")
+            )
+        date_graph.title = str(self.label)
+        for this_key in self.data_dict:
+            self._log(2, this_key)
+            i=0
+            pnts = []
+            self._log(6, self.data_dict[this_key])
+            if len(self.data_dict[this_key][0]) != len(self.data_dict[this_key][1]):
+                #print('len x:{0} y:{1}'.format(len(data_dict[this_key][0]), len(data_dict[this_key][1])))
+                calc_start = len(self.data_dict[this_key][0]) - len(self.data_dict[this_key][1])
+                #print(calc_start)
+                self.data_dict[this_key][0] = self.data_dict[this_key][0][calc_start:]
+                #print(data_dict[this_key])
+                #print('len x:{0} y:{1}'.format(len(data_dict[this_key][0]), len(data_dict[this_key][1])))
+
+            for this_date in self.data_dict[this_key][0]:
+                datetime_points = []
+                self._log(4, this_date)
+                month, day, year = this_date.split('-')
+                day = int(day)
+                month = int(month)
+                year = int('20{0}'.format(year))
+                self._log(4, "{0}-{1}-{2}".format(year, month, day))
+                this_dt = datetime(year, month, day)
+                pnts.append((this_dt, self.data_dict[this_key][1][i]))
+                i += 1
+            self._log(6, pnts)
+            self._log(6, "\n")
+
+            if '.sample' in this_key.lower():
+                import calendar
+                self._log(2, "I'm a sample")
+                #convert my datetimes to int timestamps
+                xValues = []
+                for dt in pnts:
+                    ts = calendar.timegm(dt[0].timetuple())
+                    self._log(5, 'date: {0}\ntime: {1}'.format(dt[0].strftime('%Y-%m-%d'), ts))
+                    xValues.append(ts)
+                #interpolate my timestamps
+                from scipy.interpolate import splev, splrep
+                i = 0
+                yValues = self.data_dict[this_key][1]
+                tck = splrep(xValues, yValues)
+                x2 = np.linspace(xValues[0], xValues[-1], 200)
+                y2 = splev(x2, tck)
+
+                #convert my interp values back into datetime objects
+                xy2 = []
+                i=0
+                interp_pnts = []
+                for i in range(0, len(x2)):
+                    interp_dt = datetime.utcfromtimestamp(x2[i])
+                    interp_pnts.append( (interp_dt, y2[i]) )
+                self._log(6, interp_pnts)   
+                date_graph.add(this_key, interp_pnts, show_dots=False)
+            else:
+                date_graph.add(this_key, pnts, show_dots=False)
+        try:
+            self.svg_str = date_graph.render()
+        except:
+            print(sys.exc_info())
+
+    def line_graph(self, input_json=None):
+        ##########################################################
+        ##I'm makin' me a line graph, jus like dem fancy injuneers
+        ##########################################################
+        self.load_json(input_json)
+        line_graph = pygal.XY(self.config, 
+            dynamic_print_values=True, 
+            show_x_guides=self.grid_x, 
+            show_y_guides=self.grid_y, 
+            x_label_rotation=45
+            )
+        self._log(2, 'label: {0}'.format(str(self.label)))
+        line_graph.title = str(self.label)
+        for this_key in self.data_dict:
+            self._log(5, '{0}: {1}'.format(this_key, self.data_dict[this_key][0]))
+            self._log(5, '{0}: {1}'.format(this_key, self.data_dict[this_key][1]))
+            self._log(3, 'len x: {0}. len y: {1}'.format(len(self.data_dict[this_key][0]), len(self.data_dict[this_key][1])))
+    
+            if '.sample' in this_key.lower():
+                from scipy.interpolate import splev, splrep
+                xValues = self.data_dict[this_key][0]
+                yValues = self.data_dict[this_key][1]
+                tck = splrep(xValues, yValues)
+                x2 = np.linspace(xValues[0], xValues[-1], 200)
+                y2 = splev(x2, tck)
+                xy2 = []
+                i=0
+                for i in range(0, len(x2)):
+                    xy2.append( (x2[i], y2[i]) )
+                line_graph.add(this_key, xy2, show_dots=False)
+            else:
+                xy = []
+                i = 0
+                for i in range(0, len(self.data_dict[this_key][1])):
+                    #xy.append( (data_dict[this_key][0][i], data_dict[this_key][1][i]) )
+                    xy.append( (i, self.data_dict[this_key][1][i]) )
+                #line_graph.add(this_key, data_dict[this_key][1])
+                line_graph.add(this_key, xy, show_dots=False)
+        try:
+            self.svg_str = line_graph.render()
+        except:
+            print(sys.exc_info())
+
+    def scatter_plot(self):
+        print('make a scatter plot')
+
+    def bar_graph(self):
+        print('make a bar graph')
+
+
+
+
 
 if __name__ == '__main__':
     options, args = parse_args(sys.argv[1:])
--- run_pg.py
+++ run_pg.py
@@ -4,7 +4,7 @@ import datetime
 import os
 import commands
 
-all_charts = ['sugar', 'sugar2', 'pressure', 'pressure2', 'weight', 'weight2', 'bmi2']
+all_charts = ['sugar', 'sugar2', 'sugarTime2', 'pressure', 'pressure2', 'weight', 'weight2', 'bmi2']
 
 today = datetime.date.today()
 fmt_today = '{:%Y%m%d}'.format(today)
@@ -12,13 +12,14 @@ fmt_today = '{:%Y%m%d}'.format(today)
 for this_chart in all_charts:
     chart_path = '../statsChart/{0}_{1}.json'.format(this_chart, fmt_today)
     if this_chart[-1].isdigit():
-    #    this_chart = this_chart[:-1]
+        chart_title = this_chart[:-1]
         chart_type = 'date'
     else:
+        chart_title = this_chart
         chart_type = 'line'
 
     if os.path.exists(chart_path):
-        cmd = './pgPlot.py -g {0} -f {1} --lb \'{2}\' -d --wi 8 --he 5'.format(chart_type, chart_path, this_chart.title())
+        cmd = './pgPlot.py -g {0} -f {1} --lb \'{2}\' -d --wi 8 --he 5'.format(chart_type, chart_path, chart_title.title())
         status, output = commands.getstatusoutput(cmd)
         print('status: {0}\n\noutput: {1}\n\n'.format(status, output))
     else:
